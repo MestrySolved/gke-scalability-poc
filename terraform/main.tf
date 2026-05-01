03f7c68ae4df66dff642c0f506737b05200e3062
MestrySolved <anushkamestry29@gmail.com>
2026-05-01 22:48:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,14 @@
 # --- Provider & Project Details ---
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
