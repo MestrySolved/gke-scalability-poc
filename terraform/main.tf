cc5c6bab1908a80fde0991fece18ee381dafa757
MestrySolved <anushkamestry29@gmail.com>
2026-05-01 22:51:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,4 @@
 # --- Provider & Project Details ---
-
 terraform {
   required_providers {
     google = {
@@ -24,10 +23,9 @@ resource "google_compute_network" "gke_vpc" {
 resource "google_compute_subnetwork" "gke_subnet" {
   name          = "gke-poc-subnet"
   ip_cidr_range = "10.0.0.0/20"
-  region        = "asia-south1"
+  region        = var.region
   network       = google_compute_network.gke_vpc.id
 
-  # Alias ranges for VPC-Native GKE
   secondary_ip_range {
     range_name    = "pod-ranges"
     ip_cidr_range = "10.1.0.0/16"
@@ -41,11 +39,8 @@ resource "google_compute_subnetwork" "gke_subnet" {
 # --- GKE Regional Cluster ---
 resource "google_container_cluster" "primary" {
   name     = "gke-scalability-poc"
-  location = var.region  # Using variable instead of hardcoded region
-  ...
-}
+  location = var.region
 
-  # Enabling VPC-Native Networking
   networking_mode = "VPC_NATIVE"
   network         = google_compute_network.gke_vpc.name
   subnetwork      = google_compute_subnetwork.gke_subnet.name
@@ -55,22 +50,19 @@ resource "google_container_cluster" "primary" {
     services_secondary_range_name = "service-ranges"
   }
 
-  # We create a separate node pool, so we delete the default one
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  # Security: Enable Workload Identity
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
   }
 
-  # Cost Optimization: Enable Vertical Pod Autoscaling
   vertical_pod_autoscaling {
     enabled = true
   }
 }
 
-# --- Managed Node Pool with Autoscaling ---
+# --- Managed Node Pool ---
 resource "google_container_node_pool" "primary_nodes" {
   name       = "autoscaling-node-pool"
   location   = var.region
@@ -79,19 +71,16 @@ resource "google_container_node_pool" "primary_nodes" {
 
   autoscaling {
     min_node_count = 1
-    max_node_count = 5 # Scalability for high-traffic apps
+    max_node_count = 5
   }
 
   node_config {
     machine_type = "e2-medium"
-    
-    # IAM & Security
     service_account = google_service_account.gke_sa.email
     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
 
-# --- IAM for Workload Identity ---
 resource "google_service_account" "gke_sa" {
   account_id   = "gke-poc-sa"
   display_name = "GKE PoC Service Account"
